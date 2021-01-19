a14b8a29ae96b7b9fb83dd2b55ee4b10785814b9
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-19 22:36:21 +0200

    solve Q22

--- q22.js
+++ q22.js
@@ -3,10 +3,18 @@ function run() {
     const output = document.getElementById("output");
     let out = "";
     let arr = [];
+    let arrNew = [];
     
-
-    // out = input + 5;
-
-/**/
+    // search if the number is in the arrays, if not, the number will enter the new array
+    for (let i = 0; i < arr.length;  i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] !== arrNew.includes()) {
+                arrNew.push(arr[i][j]);
+            }
+        }
+    }
+    
+    out = arrNew;
+ 
     output.innerText = out;
 }
