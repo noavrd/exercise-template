59d58c925b6f8c8771a67bfce29e2421245c047c
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-19 16:57:34 +0200

    solve Q3

--- q3.js
+++ q3.js
@@ -1,13 +1,13 @@
 function run() {
     const input = document.getElementById("input").value;
+    const inputSec = document.getElementById("input2").value; // only for question 3
     const output = document.getElementById("output");
     let out = "";
+    let arr = [];
+    for(let i = 0; i < inputSec; i++){
+        arr.push(input[i]);
+    }
+    out = arr;
 
-    output = input[0];
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
     output.innerText = out;
 }
