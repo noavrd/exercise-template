006fd313ab716f2f390ce02b6b1c7741aa8a9ade
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-19 22:20:03 +0200

    solve Q24

--- q24.js
+++ q24.js
@@ -2,10 +2,15 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+    let arr = [];
+    let newAr = [];
 
-/**/
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== null && arr[i] !== 0 && arr[i] !== undefined && arr[i] !== NaN && arr[i]) {
+            newAr.push(arr[i]);
+        }
+    }
+    out = newAr;
+
     output.innerText = out;
 }
