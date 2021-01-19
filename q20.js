5c48bd89adeaed463bcf98a5806a9135e820e2f0
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-19 22:02:42 +0200

    solve Q20

--- q20.js
+++ q20.js
@@ -2,9 +2,18 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+    let arr = [];
+    arr.push(input);
+    let dup = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < dup.length; j++) {
+            if (arr[i] !== dup[j]){
+                dup.push(arr[i]);
+            }
+        }
+    }
+     out = dup;
 
 /**/
     output.innerText = out;
