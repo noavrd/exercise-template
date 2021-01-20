be6ad90827648bb6ffb310b170b179fcccc136b0
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-20 16:54:24 +0200

    solve Q24

--- q24.js
+++ q24.js
@@ -1,16 +1,5 @@
-function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-    let arr = [];
-    let newAr = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== null && arr[i] !== 0 && arr[i] !== undefined && arr[i] !== NaN && arr[i]) {
-            newAr.push(arr[i]);
-        }
-    }
-    out = newAr;
-
-    output.innerText = out;
+function run(arr) {
+    return arr.filter(Boolean);
 }
+
+run([NaN, 0, 15, false, -22, '',undefined, 47, null])
