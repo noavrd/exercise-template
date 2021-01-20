e5877c78cf7fade109468eed0145b3afde4bd5f8
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-20 12:58:22 +0200

    solve Q6

--- q6.js
+++ q6.js
@@ -1,16 +1,13 @@
-function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-    let x =[];
-    x.push(input);
-    let arr = [];
-    for (let i = 0; i < x.length; i++){
-        if (x[i]%2 === 0 && x[i-1]) {
-                    arr.push("-");
-                }
-        arr.push(x[i]);
+function run(x) {
+    let num1 = x.toString();
+    let arr = [num1[0]];
+    for (let i = 0; i < num1.length; i++){
+        if (num1[i]%2 === 0 && num1[i-1]%2 === 0) {
+            arr.push("-",num1[i]);
+        } else {
+            arr.push(num1[i]);
+        }
     }
-    out = arr;
-    output.innerText = out;
+    console.log(arr.join(''));
 }
+run(05485665)
